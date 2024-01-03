db0291e31ef3bf961b43a04f6eb22bdf76f2fc38
developerdesigner18 <developer.designer18@gmail.com>
2024-01-03 17:05:23 +0530

    Added SQLite

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {LogBox, StatusBar} from 'react-native';
 import {Provider} from 'react-redux';
 import {LogLevel, OneSignal} from 'react-native-onesignal';
+import {openDatabase} from 'react-native-sqlite-storage';
 import SplashScreen from 'react-native-splash-screen';
 import FlashMessage from 'react-native-flash-message';
 import Navigation from './app/navigation';
@@ -10,8 +11,10 @@ import {gstyles} from './app/common/gstyles';
 import {colors} from './app/common/colors';
 import {ONESIGNAL_APP_ID} from './app/common/utils';
 
+let db = openDatabase({name: 'MiYamaha.db', createFromLocation: 1});
+
 export default class App extends Component {
-  componentDidMount() {
+  async componentDidMount() {
     setTimeout(() => {
       SplashScreen.hide();
     }, 4000);
@@ -20,10 +23,72 @@ export default class App extends Component {
     OneSignal.Debug.setLogLevel(LogLevel.Verbose);
     OneSignal.initialize(ONESIGNAL_APP_ID);
     OneSignal.Notifications.requestPermission(true);
-    OneSignal.Notifications.addEventListener('click', event => {
-      console.log('OneSignal: notification clicked:', event);
-    });
+
+    this.forgroundListener = OneSignal.Notifications.addEventListener(
+      'foregroundWillDisplay',
+      event => {
+        event.preventDefault();
+        event.getNotification().display();
+        this.saveNotifications(event.notification);
+      },
+    );
+
+    this.clickListner = OneSignal.Notifications.addEventListener(
+      'click',
+      event => {
+        this.saveNotifications(event.notification);
+      },
+    );
   }
+
+  componentWillUnmount() {
+    OneSignal.Notifications.removeEventListener('click', this.clickListner);
+    OneSignal.Notifications.removeEventListener(
+      'foregroundWillDisplay',
+      this.forgroundListener,
+    );
+  }
+
+  saveNotifications = i => {
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM Notifications',
+        [],
+        (tx, results) => {
+          this.setState({loading: false});
+          const rows = results.rows;
+          const data = [];
+          for (let i = 0; i < rows.length; i++) {
+            const user = rows.item(i);
+            data.push(user);
+          }
+          const foundObject = data.find(
+            item => item.notificationId === i.notificationId,
+          );
+          if (!foundObject) {
+            db.transaction(function (tx) {
+              tx.executeSql(
+                'INSERT INTO Notifications(notificationId, title, body, date) VALUES (?,?,?,?)',
+                [i.notificationId, i.title, i.body, new Date()],
+                (tx, results) => {
+                  console.log('Results', results.rowsAffected);
+                  if (results.rowsAffected > 0) {
+                  } else {
+                  }
+                },
+                error => {
+                  console.log(error, '===error');
+                },
+              );
+            });
+          }
+        },
+        error => {
+          console.error('Error fetching users', error);
+        },
+      );
+    });
+  };
 
   render() {
     return (
--- app/screens/Notification.js
+++ app/screens/Notification.js
@@ -1,14 +1,18 @@
 import React, {Component} from 'react';
 import {FlatList, View} from 'react-native';
 import {connect} from 'react-redux';
+import {openDatabase} from 'react-native-sqlite-storage';
+import Spinner from 'react-native-loading-spinner-overlay';
+import moment from 'moment';
 import {gstyles} from '../common/gstyles';
-import {notifications} from '../common/utils';
 import {strings as str} from '../common/strings';
 import Container from '../components/Container';
 import TopHeader from '../components/TopHeader';
 import ItemCard from '../components/ItemCard';
 import AuthButton from '../components/AuthButton';
 import Alert from '../components/Alert';
+
+let db = openDatabase({name: 'MiYamaha.db', createFromLocation: 1});
 
 class Notification extends Component {
   constructor(props) {
@@ -17,8 +21,59 @@ class Notification extends Component {
       selected: 0,
       isVisible: false,
       data: {},
+      notifications: [],
+      loading: false,
     };
   }
+  componentDidMount() {
+    this.fetchNotifications();
+  }
+
+  fetchNotifications = () => {
+    this.setState({loading:true})
+    db.transaction(tx => {
+      tx.executeSql(
+        'SELECT * FROM Notifications',
+        [],
+        (tx, results) => {
+          this.setState({loading:false})
+          const rows = results.rows;
+          const data = [];
+          for (let i = 0; i < rows.length; i++) {
+            const user = rows.item(i);
+            data.push(user);
+          }
+          this.setState({notifications: data.reverse()});
+        },
+        error => {
+          this.setState({loading:false})
+          console.error('Error fetching users', error);
+        },
+      );
+    });
+  };
+
+  removeNotification = () => {
+    this.setState({loading: true});
+    db.transaction(tx => {
+      tx.executeSql(
+        'DELETE FROM  Notifications where id=?',
+        [this.state.selected],
+        (tx, results) => {
+          console.log('Results', results.rowsAffected);
+          if (results.rowsAffected > 0) {
+            this.setState({loading: false});
+            this.fetchNotifications();
+          } else {
+            this.setState({loading: false});
+          }
+        },
+        error => {
+          this.setState({loading: false});
+        },
+      );
+    });
+  };
 
   handleSelection = i => {
     this.setState({selected: i});
@@ -34,20 +89,20 @@ class Notification extends Component {
   renderItem = ({item}) => (
     <ItemCard
       title={item.title}
-      subTitle={item.description}
+      subTitle={item.body}
       selected={this.state.selected == item.id}
       onRadioPress={() => this.handleSelection(item.id)}
-      leftLabel={item.date}
+      leftLabel={moment(item.date).format('DD/MM/YY')}
       onPress={() => this.handleItemPress(item)}
     />
   );
 
   render() {
-    const {isVisible, data} = this.state;
+    const {isVisible, data, notifications, loading} = this.state;
     return (
       <Container style={{paddingHorizontal: 10}}>
         <TopHeader label={str.notifications} />
-        <View style={gstyles.listContainer}>
+        <View style={[gstyles.listContainer, {marginBottom:'40%'}]}>
           <FlatList
             data={notifications}
             keyExtractor={item => item.id}
@@ -62,16 +117,18 @@ class Notification extends Component {
             {opacity: this.state.selected == 0 ? 0.6 : 1},
           ]}
           disabled={this.state.selected == 0}
+          onPress={this.removeNotification}
         />
         <Alert
           visible={isVisible}
           isInfo
           btnTitle={str.close}
           title={data.title}
-          subTitle={data.description}
-          rightLabel={data.date}
+          subTitle={data.body}
+          rightLabel={moment(data.date).format('DD/MM/YY')}
           onSubmit={this.handleItemPress}
         />
+        <Spinner visible={loading} />
       </Container>
     );
   }
